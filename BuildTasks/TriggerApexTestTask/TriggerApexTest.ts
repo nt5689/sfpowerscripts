46172c0965d5a048fc453ac4e10512af2db83f90
azlamsalam <azlam.salam@outlook.com>
2019-12-28 01:16:27 +1100

    Updated Apex Test

--- BuildTasks/TriggerApexTestTask/TriggerApexTest.ts
+++ BuildTasks/TriggerApexTestTask/TriggerApexTest.ts
@@ -2,6 +2,8 @@ import tl = require("azure-pipelines-task-lib/task");
 import child_process = require("child_process");
 import TriggerApexTestImpl from "./TriggerApexTestImpl";
 import { AppInsights } from "../Common/AppInsights";
+import path = require("path");
+import os = require("os");
 
 async function run() {
   try {
@@ -13,18 +15,29 @@ async function run() {
     test_options["wait_time"] = tl.getInput("wait_time", true);
 
     test_options["testlevel"] = tl.getInput("testlevel", true);
+    test_options["synchronous"] = tl.getBoolInput("synchronous", false);
+
+
 
     if (test_options["testlevel"] == "RunSpecifiedTests")
     test_options["specified_tests"] = tl.getInput("specified_tests", true);
     if (test_options["testlevel"] == "RunApexTestSuite")
     test_options["apextestsuite"] = tl.getInput("apextestsuite", true);
+   
+
+    let stagingDir: string = path.join(
+      tl.getVariable("build.artifactStagingDirectory"),
+      ".testresults"
+    );
+
+    test_options['outputdir']=stagingDir;
+
 
     let triggerApexTestImpl:TriggerApexTestImpl = new TriggerApexTestImpl(target_org,test_options);
-    console.log("Generating command");
-    let command = await triggerApexTestImpl.buildExecCommand();
-    tl.debug(command);
- 
-    await triggerApexTestImpl.exec(command);
+    console.log("Executing command");
+    await triggerApexTestImpl.exec();
+
+    publishTestResults(test_options['outputdir']);
 
     AppInsights.trackTask("sfpwowerscript-triggerapextest-task");
     AppInsights.trackTaskEvent("sfpwowerscript-triggerapextest-task","apex_test_triggered");    
@@ -32,6 +45,19 @@ async function run() {
   } catch (err) {
     AppInsights.trackExcepiton("sfpwowerscript-triggerapextest-task",err);    
     tl.setResult(tl.TaskResult.Failed, err.message);
+  }
+}
+
+function publishTestResults(resultsDir: string): void {
+  const buildConfig = tl.getVariable('BuildConfiguration');
+  const buildPlaform = tl.getVariable('BuildPlatform');
+  const testRunTitle = "Apex Test Run";
+  const matchingTestResultsFiles: string[] = tl.findMatch(resultsDir, '*-junit.xml');
+  if (!matchingTestResultsFiles || matchingTestResultsFiles.length === 0) {
+      tl.warning('No test result files were found.');
+  } else {
+      const tp: tl.TestPublisher = new tl.TestPublisher('JUnit');
+      tp.publish(matchingTestResultsFiles, 'false', buildPlaform, buildConfig, testRunTitle, 'true', "sfpowerscripts-apextests");
   }
 }
 
--- BuildTasks/TriggerApexTestTask/TriggerApexTestImpl.ts
+++ BuildTasks/TriggerApexTestTask/TriggerApexTestImpl.ts
@@ -1,63 +1,91 @@
 import child_process = require("child_process");
 import { onExit } from "../Common/OnExit";
 
-
-
 export default class TriggerApexTestImpl {
+  public constructor(private target_org: string, private test_options: any) {}
 
-    public constructor(
-        private target_org: string,
-        private test_options: any
-      ) {}
+  public async exec(): Promise<void> {
+    let testExecCommand = this.buildTestExecCommand();
 
+    let result = child_process.execSync(testExecCommand, {
+      encoding: "utf8",
+      stdio: ["pipe", "pipe", "ignore"]
+    });
 
-      public async exec(command:string):Promise<void>
-      {
-        let child=child_process.exec(command,  { encoding: "utf8" },(error,stdout,stderr)=>{
+    let resultAsJSON = JSON.parse(result);
+    let testRunId: string;
+    if (resultAsJSON.status == 0) {
+      testRunId = resultAsJSON.result.testRunId;
+    } else throw new Error("Triggering Apex Test Run Failed");
 
-          if(error)
-             throw error;
-        });
-       
-        child.stdout.on("data",data=>{console.log(data.toString()); });
-
-        await onExit(child);
-
+    //First Lets print in Human for human readable
+    let child = child_process.exec(
+      this.buildTestReportCommand(testRunId,'human'),
+      { encoding: "utf8" },
+      (error, stdout, stderr) => {
+        if (error) throw error;
       }
+    );
+
+    child.stdout.on("data", data => {
+      console.log(data.toString());
+    });
+
+    await onExit(child);
+
+    
+    //Reprint the report in junit and make it silent
+     child = child_process.exec(
+      this.buildTestReportCommand(testRunId,'junit'),
+      { encoding: "utf8" },
+      (error, stdout, stderr) => {
+        if (error) throw error;
+      }
+    );
+    await onExit(child);
+
+  }
+
+  private buildTestExecCommand(): string {
+    let command = `npx sfdx force:apex:test:run -u ${this.target_org}`;
+
+    //output
+    command += ` --json`;
+
+    if(this.test_options['synchronous']==true)
+    command += ` -y`;
+
+    //testlevel
+    command += ` -l ${this.test_options["testlevel"]}`;
+
+    if (this.test_options["testlevel"] == "RunSpecifiedTests") {
+      command += ` -t ${this.test_options["specified_tests"]}`;
+    } else if (this.test_options["testlevel"] == "RunApexTestSuite") {
+      command += ` -s ${this.test_options["apextestsuite"]}`;
+    }
+    console.log(`Generated Command: ${command}`)
+    return command;
+  }
+
+  private buildTestReportCommand(testRunId: string, format:string): string {
+    let command = `npx sfdx force:apex:test:report -u ${this.target_org}`;
+
+    command += ` -r ${format}`;
+
+    command += ` -c`;
+
+    //testid
+    command += ` -i ${testRunId}`;
+
+    //wait time
+    command += ` -w  ${this.test_options["wait_time"]}`;
+
+    //store result
+    command += ` -d  ${this.test_options["outputdir"]}`;
+
+    console.log(`Generated Command: ${command}`)
+    return command;
+  }
+
   
-      public async buildExecCommand(): Promise<string> {
-       
-    
-        let command = `npx sfdx force:apex:test:run -u ${this.target_org}`;
-    
-
-        command += ` -c`;
-    
-        //output
-        command += ` -r human`;
-    
-        //testlevel
-        command += ` -l ${this.test_options["testlevel"]}`;
-    
-   
-        if (this.test_options["testlevel"] == "RunSpecifiedTests") {
-         
-          command += ` -t ${this.test_options["specified_tests"]}`;
-
-        } 
-        else if (this.test_options["testlevel"] == "RunApexTestSuite") {
-        
-          command += ` -s ${this.test_options["apextestsuite"]}`;
-        }
-
-         //wait
-         command += ` -w ${this.test_options["wait_time"]}`;
-    
-    
-         command += ` --verbose`;
-
-        return command;
-      }
-    
-     
 }
