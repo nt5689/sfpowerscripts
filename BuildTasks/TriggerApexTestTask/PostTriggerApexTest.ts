0563157d9120f3ba48c7fa2c52c07c9636a0a8d1
azlamsalam <azlam.salam@outlook.com>
2019-12-29 00:58:17 +1100

    Fix for duplicate

--- BuildTasks/TriggerApexTestTask/PostTriggerApexTest.ts
+++ BuildTasks/TriggerApexTestTask/PostTriggerApexTest.ts
@@ -1,11 +1,10 @@
 import tl = require("azure-pipelines-task-lib/task");
 import path = require("path");
+import fs = require("fs-extra");
 
 
 async function run() {
 
-  
-  
   let stagingDir: string = path.join(
     tl.getVariable("build.artifactStagingDirectory"),
     ".testresults"
@@ -19,11 +18,11 @@ function publishTestResults(resultsDir: string): void {
   const buildPlaform = tl.getVariable('BuildPlatform');
   const testRunTitle = "Apex Test Run";
   const matchingTestResultsFiles: string[] = tl.findMatch(resultsDir, '*-junit.xml');
-  if (!matchingTestResultsFiles || matchingTestResultsFiles.length === 0) {
-      tl.warning('No test result files were found.');
-  } else {
+  if (matchingTestResultsFiles && matchingTestResultsFiles.length > 0) {
       const tp: tl.TestPublisher = new tl.TestPublisher('JUnit');
       tp.publish(matchingTestResultsFiles, 'true', buildPlaform, buildConfig, testRunTitle, 'true', "sfpowerscripts-apextests");
+      //Remove the directory, so that other post execution scripts dont upload the same test results again and duplicate
+      fs.removeSync(resultsDir);
   }
 }
 
